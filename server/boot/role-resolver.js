82b48ddf7f02b02298b01f86d37f8f73fe1452cc
Flpichon <franckiiboss@gmail.com>
2019-12-16 19:18:56 +0100

    wip back

---
+++ server/boot/role-resolver.js
@@ -0,0 +1,7 @@
+module.exports = function(app) {
+    const Role = app.models.Role;
+    // Role.registerResolver('admin', function (role, ctx, cb) {
+    //     console.log(ctx);
+    //     console.log(role);
+    // });
+}
